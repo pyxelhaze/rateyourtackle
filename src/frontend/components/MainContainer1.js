e08fefb85ecdc785573a1e693598278916b4452c
pyxelhaze <110282836+pyxelhaze@users.noreply.github.com>
2024-08-07 09:23:50 +0200

    maincontainer structure

--- src/frontend/components/MainContainer1.js
+++ src/frontend/components/MainContainer1.js
@@ -1,7 +1,17 @@
 const MainContainer1 = () => {
     return (<div
         className="h-1/3 w-full border-2 rounded border-blue-400">
+
         <h3 className="p-2 text-orange-600 font-bold text-xl">Top-Tackle</h3>
+        <div className="flex flex-row h-full">
+            <div className="h-2/3 w-1/5 mx-3 border-2 rounded border-blue-400"></div>
+            <div className="h-2/3 w-1/5 mx-3 border-2 rounded border-blue-400"></div>
+            <div className="h-2/3 w-1/5 mx-3 border-2 rounded border-blue-400"></div>
+            <div className="h-2/3 w-1/5 mx-3 border-2 rounded border-blue-400"></div>
+            <div className="h-2/3 w-1/5 mx-3 border-2 rounded border-blue-400"></div>
+
+        </div>
+
     </div>);
 }
 
--- src/frontend/components/MainContainer2.js
+++ src/frontend/components/MainContainer2.js
@@ -2,6 +2,12 @@ const MainContainer2 = () => {
     return (<div
         className="h-full w-3/5 border-2 rounded border-blue-400">
         <h3 className="p-2 text-orange-600 font-bold text-xl">New Ratings</h3>
+        <div className="flex flex-col items-center h-full w-full px-2">
+            <div className="h-1/4 w-full my-1  border-2 rounded border-blue-400"></div>
+            <div className="h-1/4 w-full my-1  border-2 rounded border-blue-400"></div>
+            <div className="h-1/4 w-full my-1  border-2 rounded border-blue-400"></div>
+
+        </div>
     </div>);
 }
 
